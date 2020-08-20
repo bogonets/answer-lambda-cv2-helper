285fd84cbaaa0e18d914575ead7ff0fd715b3197
sunhongyi <sunhongyi@gmail.com>
2020-08-20 13:56:49 +0900

    [Add Lambda] get bounding box roi

---
+++ cv2_helper_bounding_roi.app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def on_run(roi):
+    if roi.size < 4:
+        raise ValueError("roi length should be greater than 4.")
+
+    x_list = roi[::, 0::2]
+    y_list = roi[::, 1::2]
+
+    return {"result": np.array([np.min(x_list), np.max(x_list), np.min(y_list), np.max(y_list)])}
+
+
+if __name__ == '__main__':
+    pass
